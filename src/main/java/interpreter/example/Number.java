0323373a854a6b4ac06c428204518f625030e5b3
MauriVargas17 <vargasmauricio65@gmail.com>
2022-05-27 00:16:48 -0400

    Ejemplo Interpreter

---
+++ src/main/java/interpreter/example/Number.java
@@ -0,0 +1,15 @@
+package interpreter.example;
+
+public class Number extends OperationM {
+
+    public Number(int digit){
+        this.digit=digit;
+    }
+
+    @Override
+    public void interpreter(Context context) {
+        System.out.println("Digit from Number: "+digit);
+        context.output= context.output+(Integer.parseInt(context.input.substring(0,this.digit))*context.signo);
+        context.input= context.input.substring(this.digit);
+    }
+}
