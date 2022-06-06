a8917eece7863426d18087c3f5ebe515d12aab3b
MauriVargas17 <vargasmauricio65@gmail.com>
2022-06-05 21:15:44 -0400

    Chain of Responsibility

--- src/main/java/behavioralPatterns/chainOfResponsibility/practice/Client.java
+++ src/main/java/behavioralPatterns/chainOfResponsibility/practice/Client.java
@@ -5,5 +5,9 @@ public class Client {
         ComplexityManager cm = new ComplexityManager();
         int[] arr = {2,4,34,5,6,6,34,3,2,3,45,4,4,5,4,24,53,46,456,456,45,6,56,4,7,8,78,70};
         cm.criteriaHandler(arr);
+        int[] arr1 = {2,4,34,5,6,6,34,3,2,3,45,4,4,5,4,24,53,3,4,5,645,625,62,6245,16,467,568,567845,746,8374,673,34,645,467,458,457,845,67,34,6735,6,3645,72,47,4,46,456,456,45,6,56,4,7,8,78,70};
+        cm.criteriaHandler(arr1);
+        int[] arr2 = {1,4,6,3,5,7,0};
+        cm.criteriaHandler(arr2);
     }
 }
